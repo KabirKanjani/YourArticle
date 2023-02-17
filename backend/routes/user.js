8096091f2ed6cd76e619d092a3832692dc7ee3da
Kabir11 <andrewkabir@gmail.com>
2023-02-17 22:53:52 +0530

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const router=express.Router();
-const userController=require('../controllers/user_controller')
+const userController=require('../controllers/user_controller.js')
 //Login Route
 console.log("Users")
 router.post("/login",userController.loginUser)
